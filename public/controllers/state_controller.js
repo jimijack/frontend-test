54cc9ef11a9f4ae20a7e64f4252f81e171710862
James Ewing <olearysound@gmail.com>
2016-01-24 19:25:32 -0800

    html tag with statesApp, edited icon script tags

--- public/controllers/state_controller.js
+++ public/controllers/state_controller.js
@@ -1,1 +1,12 @@
-controller.js
+(function() {
+
+  'use strict';
+
+  var statesApp = angular.module('statesApp', []);
+        statesApp.controller('StatesController', function ($scope, $http){
+          $http.get('states.json').success(function(data) {
+            $scope.states = data;
+          });
+        });
+
+})();
