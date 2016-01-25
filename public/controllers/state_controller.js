b0fa6a7074ae6b37afe4a32439653c137451c2df
James Ewing <olearysound@gmail.com>
2016-01-25 13:16:33 -0800

    Adjusted state_controller, removed 2nd parameter in module. Also fixed syntax in navbar ui-sref=states

--- public/controllers/state_controller.js
+++ public/controllers/state_controller.js
@@ -2,7 +2,7 @@
 
   'use strict';
 
-  var statesApp = angular.module('statesApp', []);
+  var statesApp = angular.module('statesApp');
         statesApp.controller('StatesController', function ($scope, $http){
           $http.get('states.json').success(function(data) {
             $scope.states = data;
