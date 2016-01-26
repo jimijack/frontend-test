6c6822ef24e964c3a20941a8e46dd8abdf03060c
James Ewing <olearysound@gmail.com>
2016-01-25 17:57:19 -0800

    Adjusted state controller to html.get /state.

--- public/controllers/state_controller.js
+++ public/controllers/state_controller.js
@@ -2,11 +2,38 @@
 
   'use strict';
 
-  var statesApp = angular.module('statesApp');
-        statesApp.controller('StatesController', function ($scope, $http){
-          $http.get('states.json').success(function(data) {
-            $scope.states = data;
-          });
-        });
+  //var statesApp = angular.module('statesApp');
+  //      statesApp.controller('StatesController', function ($scope, $http){
+  //        $http.get('/states.json').success(function(data) {
+  //          $scope.states = data.states;
+  //        });
+  //      });
+
+  angular
+      .module("statesApp")
+      .controller("StatesController", StatesController);
+
+  function StatesController($scope, $http) {
+
+    $scope.states = [];
+    $http.get('/states').success(function(data) {
+      console.log("success!");
+      return data.states;
+      $scope.states = data.states;
+        console.log(data.states);
+    });
+
+  };
 
 })();
+
+
+
+//menuApp.controller("dynamicMenuController", function($scope, $http) {
+//$scope.appetizers= [];
+//$http.get('config/menu.json').success(function(data) {
+//    console.log("success!");
+//    $scope.appetizers = data.appetizers;
+//        console.log(data.appetizers);
+//    });
+//});
